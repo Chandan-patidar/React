e4f229424adc51fc173906f05ecc73eca14bd2fb
Chandan Patidar <88586932+Chandan-patidar@users.noreply.github.com>
2024-12-08 17:53:35 +0530

    dynamic attributes

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ function add(x,y){
     return x+y ;
 }
 function MyComponent(){
+    const link = "https://www.youtube.com/@ChandarPatidar ";
+    return <a href={link}> Click Me </a>;
   /*  const name = "chandan patidar "
-    const x = 2+3 ;*/
-    return <h2> Hello { add(10,5)} </h2> ;
+    const x = 2+3 ;
+    return <h2> Hello { add(10,5)} </h2> ;*/
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
