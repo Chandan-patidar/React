a2dc5e03a662d390bb1e956390fc0253e12a01ba
Chandan Patidar <88586932+Chandan-patidar@users.noreply.github.com>
2024-12-08 18:02:14 +0530

    new card function  created

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function add(x,y){
-    return x+y ;
-}
-function MyComponent(){
-    const link = "https://www.youtube.com/@ChandarPatidar ";
-    return <a href={link}> Click Me </a>;
-  /*  const name = "chandan patidar "
-    const x = 2+3 ;
-    return <h2> Hello { add(10,5)} </h2> ;*/
+function Card(){
+    return <h1> Card </h1>;
+
 }
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MyComponent/>);
+root.render(<Card/>);
